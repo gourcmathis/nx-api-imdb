b6fcf0152022bf3015dd51f375c926a3725be310
Dmytro Matsepa <92322153+dimanlm@users.noreply.github.com>
2022-04-03 20:00:05 +0200

    Structure change

--- src/api/imdb.py
+++ src/api/imdb.py
@@ -10,12 +10,8 @@ async def call_searchByTitle(url:str, api_key: str, title: str):
     res= await client.get(url+api_key+"/"+title)
     return res.json()
 
-async def call_TOP100(url: str, api_key: str):
-    res = await client.get(url+"/"+api_key)
-    return res.json()
-
-async def call_TOP250IMDB(url:str, api_key: str):
-    res= await client.get(url+api_key+"?groups=top_250&count=250")
+async def call_TOP100IMDB(url:str, api_key: str):
+    res= await client.get(url+api_key+"?groups=top_250&count=100")
     return res.json()
 
 async def call_get_trailer(url:str, api_key: str, id: str):
--- src/api/v1/endpoints.py
+++ src/api/v1/endpoints.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI, APIRouter
-from pydantic import BaseModel
 from dotenv import load_dotenv 
-from src.imdb import *
 import json
 import os
-import src.requestsimdb as requestsimdb
+import src.api.imdb as imdb
+from ..constants import *
 
 load_dotenv()
 API_key=os.getenv("API_key")
@@ -16,32 +15,25 @@ router = APIRouter(
 
 @router.get("/usage", tags=["API Usage"])
 async def usage():
-    return (await requestsimdb.call_usage(API_USAGE, API_key))
+    return (await imdb.call_usage(API_USAGE, API_key))
 
 @router.get("/search/{title}", tags=["Search films by name"])
 async def searchByTitle(title: str):
-    request = await requestsimdb.call_searchByTitle(API_SEARCH_TITLE, API_key, title)
+    request = await imdb.call_searchByTitle(API_SEARCH_TITLE, API_key, title)
     dictionary = json.dumps(request)
     films = json.loads(dictionary)
     return(films["results"])
 
 @router.get("/get_trailer/{id}", tags=["Get trailer of a film"])
 async def get_trailer(id: str):
-    request = await requestsimdb.call_get_trailer(API_GET_TRAILER, API_key, id)
+    request = await imdb.call_get_trailer(API_GET_TRAILER, API_key, id)
     dictionary = json.dumps(request)
     films = json.loads(dictionary)
     return(films["videoUrl"])
 
-@router.get("/get_films", tags=["Get a set of 100 films"])
-async def get100MostPopularFilms():
-    request = await requestsimdb.call_TOP100(API_TOP100, API_key)
-    dictionary = json.dumps(request)
-    films = json.loads(dictionary)
-    return(films["items"])
-
-@router.get("/get_250", tags=["Get a set of TOP 250 films"])
-async def getTOP250IMDBfilms():
-    request = await requestsimdb.call_TOP250IMDB(API_ADVANCED,API_key)
+@router.get("/get_100", tags=["Get a set of TOP 100 films"])
+async def getTOP100IMDBfilms():
+    request = await imdb.call_TOP100IMDB(API_ADVANCED,API_key)
     dictionary = json.dumps(request)
     films = json.loads(dictionary)
     return(films["results"])
